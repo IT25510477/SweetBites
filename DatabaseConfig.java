9eea717dd1a264c8beba1983b598e229de5cc436
IT25103650 <it25103650@my.sliit.lk>
2026-05-18 19:45:46 +0530

    chore: remove misplaced root-level files

--- DatabaseConfig.java
+++
@@ -1,9 +0,0 @@
-package com.bakery.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DatabaseConfig {
-    // Database configuration handled via application.properties
-    // All data access is performed through Spring Data JPA Repository interfaces
-}
