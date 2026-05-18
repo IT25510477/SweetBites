358ecbbcb0eb5c45be94b786daca8ed4475bc221
IT25101714 <it25101714@my.sliit.lk>
2026-05-18 10:55:28 +0530

    Common Files

---
+++ Displayable.java
@@ -0,0 +1,10 @@
+package com.bakery.model;
+
+/**
+ * INTERFACE demonstrating polymorphism.
+ * Any entity that can be displayed on the UI implements this.
+ */
+public interface Displayable {
+    String getDisplayName();
+    String getDisplayDescription();
+}
